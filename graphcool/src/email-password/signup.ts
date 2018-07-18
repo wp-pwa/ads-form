4359eb55a21fee77aceb049d79ee28471b0e6e08
Luis Herranz <luisherranz@gmail.com>
2018-07-18 13:10:12 +0200

    fix(signup): modify handlers to include name

--- graphcool/src/email-password/signup.ts
+++ graphcool/src/email-password/signup.ts
@@ -1,13 +1,14 @@
-import { fromEvent, FunctionEvent } from 'graphcool-lib'
-import { GraphQLClient } from 'graphql-request'
-import * as bcrypt from 'bcryptjs'
-import * as validator from 'validator'
+import {fromEvent, FunctionEvent} from "graphcool-lib"
+import {GraphQLClient} from "graphql-request"
+import * as bcrypt from "bcryptjs"
+import * as validator from "validator"
 
 interface User {
   id: string
 }
 
 interface EventData {
+  name: string
   email: string
   password: string
 }
@@ -19,19 +20,20 @@ export default async (event: FunctionEvent<EventData>) => {
 
   try {
     const graphcool = fromEvent(event)
-    const api = graphcool.api('simple/v1')
+    const api = graphcool.api("simple/v1")
 
-    const { email, password } = event.data
+    const {name, email, password} = event.data
 
     if (!validator.isEmail(email)) {
-      return { error: 'Not a valid email' }
+      return {error: "Not a valid email"}
     }
 
     // check if user exists already
-    const userExists: boolean = await getUser(api, email)
-      .then(r => r.User !== null)
+    const userExists: boolean = await getUser(api, email).then(
+      r => r.User !== null
+    )
     if (userExists) {
-      return { error: 'Email already in use' }
+      return {error: "Email already in use"}
     }
 
     // create password hash
@@ -39,19 +41,19 @@ export default async (event: FunctionEvent<EventData>) => {
     const hash = await bcrypt.hash(password, salt)
 
     // create new user
-    const userId = await createGraphcoolUser(api, email, hash)
+    const userId = await createGraphcoolUser(api, name, email, hash)
 
     // generate node token for new User node
-    const token = await graphcool.generateNodeToken(userId, 'User')
+    const token = await graphcool.generateNodeToken(userId, "User")
 
-    return { data: { id: userId, token } }
+    return {data: {id: userId, token}}
   } catch (e) {
     console.log(e)
-    return { error: 'An unexpected error occured during signup.' }
+    return {error: e.message}
   }
 }
 
-async function getUser(api: GraphQLClient, email: string): Promise<{ User }> {
+async function getUser(api: GraphQLClient, email: string): Promise<{User}> {
   const query = `
     query getUser($email: String!) {
       User(email: $email) {
@@ -61,16 +63,22 @@ async function getUser(api: GraphQLClient, email: string): Promise<{ User }> {
   `
 
   const variables = {
-    email,
+    email
   }
 
-  return api.request<{ User }>(query, variables)
+  return api.request<{User}>(query, variables)
 }
 
-async function createGraphcoolUser(api: GraphQLClient, email: string, password: string): Promise<string> {
+async function createGraphcoolUser(
+  api: GraphQLClient,
+  name: string,
+  email: string,
+  password: string
+): Promise<string> {
   const mutation = `
-    mutation createGraphcoolUser($email: String!, $password: String!) {
+    mutation createGraphcoolUser($name: String!, $email: String!, $password: String!) {
       createUser(
+        name: $name,
         email: $email,
         password: $password
       ) {
@@ -80,10 +88,12 @@ async function createGraphcoolUser(api: GraphQLClient, email: string, password: 
   `
 
   const variables = {
+    name,
     email,
-    password: password,
+    password: password
   }
 
-  return api.request<{ createUser: User }>(mutation, variables)
+  return api
+    .request<{createUser: User}>(mutation, variables)
     .then(r => r.createUser.id)
 }
