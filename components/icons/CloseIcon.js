4c761fe4189131f1c2f48d761844b13936be4a1d
David Arenas <darensr@gmail.com>
2018-10-02 16:49:01 +0200

    refactor(components): create components for icons

---
+++ components/icons/CloseIcon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CloseIcon = ({ size }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-x"
+  >
+    <line x1="18" y1="6" x2="6" y2="18" />
+    <line x1="6" y1="6" x2="18" y2="18" />
+  </svg>
+);
+
+CloseIcon.propTypes = {
+  size: PropTypes.number,
+};
+
+CloseIcon.defaultProps = {
+  size: 24,
+};
+
+export default CloseIcon;
